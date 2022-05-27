9ccfc6180c8d1c789dc096465e7bd4a9b2c1277c
Ayoub Bouras <aybouras@e2r8p7.1337.ma>
2022-05-27 18:26:53 +0100

    loading...

--- srcs/requirements/wordpress/tools/update_wp-config.sh
+++ srcs/requirements/wordpress/tools/update_wp-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
-sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
-sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$DB_USER/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wordpress/wp-config.php
 
 # echo ${DB_NAME} > FILE
 # echo ${DB_USER} >> FILE
