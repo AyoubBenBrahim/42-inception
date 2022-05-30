241d88432adc30018f178fb1bed2f7ac3aedd9a2
Ayoub Bouras <aybouras@e2r9p7.1337.ma>
2022-05-30 10:21:49 +0100

    loading...

--- srcs/requirements/mariadb/tools/launch_db.sh
+++ srcs/requirements/mariadb/tools/launch_db.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-service mysql start
+# service mysql start
+/etc/init.d/mysql start
 
-mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -u root -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}'"
 mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root wordpress  < /wordpress.sql
-mysql -u root -e "USE wordpress; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-mysql -u root -e "alter user 'root'@'localhost' identified by 'password'";
+mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}'"
+mysql -u root -e "FLUSH PRIVILEGES"
+
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'";
+mysql -u root -e "FLUSH PRIVILEGES"
+
+# mysql -u root wordpress  < /wordpress.sql
+# mysql --user="root" --database="wordpress" --password="${MYSQL_ROOT_PASSWORD}" < /wordpress.sql
+
+/etc/init.d/mysql restart
+
+
+# mysql -u root -e "CREATE USER 'ayoub'@'localhost' IDENTIFIED BY '123'"
+# SET PASSWORD FOR 'root'@localhost = PASSWORD("root");
