787439afc11ab476485f85290d2eb93e7f28d785
Ayoub Bouras <aybouras@e2r9p9.1337.ma>
2022-05-31 15:52:54 +0100

    loading...

--- srcs/requirements/mariadb/tools/launch_db.sh
+++ srcs/requirements/mariadb/tools/launch_db.sh
@@ -4,10 +4,12 @@
 /etc/init.d/mysql start
 
 echo "--------------------------------"
-
-echo "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON * . * TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
+# The wildcard as host implies that the user can connect from any client host
+echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON * . * TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | mysql -u root
 echo "FLUSH PRIVILEGES" | mysql -u root
+
+# SHOW GRANTS FOR ayoub@localhost;
 
 echo "--------------------------------"
 
@@ -17,6 +19,7 @@ echo "--------------------------------"
 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -h 127.0.0.1 -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
+
 
 echo "--------------------------------"
 
