7a682fc96933b4655c3050f8c6bd5c1962da0ce0
ayoubrs <ayoub@example.com>
2022-06-11 19:11:35 +0100

    ftp fixed...

--- srcs/requirements/bonus/ftp/tools/launch_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/launch_ftp.sh
@@ -6,24 +6,6 @@
 # echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd 
 
 
-# if id -u "$FTP_USER" >/dev/null 2>&1; then
-#     echo "-----------\033[0;32ftp: user $FTP_USER exist-----\033[0m"
-# else
-#     rm /etc/vsftpd.conf
-#     cp /vsftpd.conf /etc/vsftpd.conf
-#     useradd -p $(openssl passwd -1 $FTP_PWD) $FTP_USER
-#     mkdir -p /home/$FTP_USER/ftp
-#     chown nobody:nogroup /home/$FTP_USER/ftp
-#     chmod a-w /home/$FTP_USER/ftp
-#     mkdir /home/$FTP_USER/ftp/files
-#     chown $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
-#     echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
-#     echo "secure_chroot_dir=/home/$FTP_USER/ftp/files" | tee -a /etc/vsftpd.conf
-# fi
-
-
-# USERID="$1"
-
 /bin/egrep  -i "^${FTP_USER}:" /etc/passwd
 if [ $? -eq 0 ]; then
     echo "-----------\033[0;32ftp: User $FTP_USER exists in /etc/passwd-----\033[0m"
@@ -39,9 +21,6 @@ else
     echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
     echo "secure_chroot_dir=/home/$FTP_USER/ftp/files" | tee -a /etc/vsftpd.conf
 fi
-
-
-
 
 /usr/sbin/vsftpd /etc/vsftpd.conf
 
